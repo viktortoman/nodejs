c530135ffc2ef9f3683277a6a1c0071f1f6246a4
vtomanInstructure <viktor.toman@instructure.com>
2023-10-27 15:48:07 +0200

    Init projects

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,52 @@
+const http = require('http');
+const url = require('url');
+const path = require("path");
+const fs = require('fs');
+
+const replaceTemplate = require('./modules/replaceTemplate');
+
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const productData = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+    const { query, pathname } = url.parse(req.url, true);
+
+    if (pathname === '/' || pathname === '/overview') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        const cardsHtml = productData.map(el => replaceTemplate(templateCard, el)).join('');
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+        res.end(output);
+    } else if (pathname === '/product') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        const product = productData[query.id];
+        const output = replaceTemplate(templateProduct, product);
+
+        res.end(output);
+    } else if (pathname === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+});
+
