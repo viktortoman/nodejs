e6477791ccb0b3d9884016a5de9713064aebacc8
Viktor <viktor.toman19@gmail.com>
2023-10-30 15:14:57 +0100

    add: update endpoint

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -62,6 +62,24 @@ app.get('/api/v1/tours/:id', (req, res) => {
     });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if (!tour) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+
+    res.status(200).json({
+        status: 'success',
+        data: {tour: '<Updated tour here...>'}
+    });
+});
+
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
