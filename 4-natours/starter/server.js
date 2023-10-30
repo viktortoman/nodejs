cb7ff5c9a4d792d41abd7caea84a45eaa48ed393
Viktor <viktor.toman19@gmail.com>
2023-10-30 17:31:47 +0100

    add: mongoose

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,5 +1,12 @@
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
 dotenv.config({path: "./config.env"})
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+}).then(() => {
+    console.log("DB connection successful!");
+})
 
 const app = require("./app");
 const port = process.env.PORT || 3000;
