c77fb72f6051f9abd4019ab3fd242f613879e9c9
Viktor <viktor.toman19@gmail.com>
2023-10-30 16:43:31 +0100

    add: serving static files

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -9,6 +9,7 @@ const app = express();
 // 1) MIDDLEWARES
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
