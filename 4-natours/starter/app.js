a165b052973c0794c27b409df8408245ce97cf51
Viktor <viktor.toman19@gmail.com>
2023-10-30 15:05:13 +0100

    add: handle url parameter

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -45,6 +45,23 @@ app.post('/api/v1/tours', (req, res) => {
     });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id);
+
+    if (!tour) {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {tour}
+    });
+});
+
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
 });
