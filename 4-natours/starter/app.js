4badb4fba98eb4d9f563e0e1776480b25e43424d
Viktor <viktor.toman19@gmail.com>
2023-10-30 15:01:05 +0100

    add: express post data & write into file

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,12 +1,49 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.status(200).json({message: 'Hello from the server side!', app: 'Natours'});
+// });
+//
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint...');
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.json({
+        status: 'success',
+        results: tours.length,
+        data: {tours}
+    });
 });
 
+app.post('/api/v1/tours', (req, res) => {
+    const newID = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({id: newID}, req.body);
+
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        if (err) {
+            res.status(500).json({
+                status: 'fail',
+                message: 'Error writing file'
+            });
+        }
+
+        res.status(201).json({
+            status: 'success',
+            data: {tour: newTour}
+        });
+    });
+});
 
 app.listen(port, () => {
     console.log(`App running on port ${port}...`);
