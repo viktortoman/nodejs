7e23683bdd366cfe1e9ba5baa2232f3121e8e1a9
vtomanInstructure <viktor.toman@instructure.com>
2023-10-31 17:16:31 +0100

    add: alias route

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,5 +1,13 @@
 const Tour = require('./../models/tourModel');
 
+const aliasTopTours = (req, res, next) => {
+    req.query.limit = '5';
+    req.query.sort = '-ratingsAverage,price';
+    req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+
+    next();
+};
+
 const getAllTours = async (req, res) => {
     try {
         const queryObj = {...req.query};
@@ -133,4 +141,5 @@ module.exports = {
     createTour,
     updateTour,
     deleteTour,
+    aliasTopTours
 }
