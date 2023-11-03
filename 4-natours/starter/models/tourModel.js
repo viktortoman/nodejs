2bf98d78f2b35146183c6ba688f357f5488b44e1
Viktor <viktor.toman19@gmail.com>
2023-11-03 13:25:34 +0100

    add: virtual property

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -52,6 +52,13 @@ const tourSchema = new mongoose.Schema({
         select: false
     },
     startDates: [Date]
+}, {
+    toJSON: {virtuals: true},
+    toObject: {virtuals: true}
+});
+
+tourSchema.virtual("durationWeeks").get(function () {
+    return this.duration / 7;
 });
 
 const Tour = mongoose.model("Tour", tourSchema);
