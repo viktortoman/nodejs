f548cebc65e96156e22286be6e43bac0c6989670
Viktor <viktor.toman19@gmail.com>
2023-11-03 12:29:59 +0100

    add: aggregation pipeline (match, group)

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -5,12 +5,14 @@ const {
     updateTour,
     getTour,
     deleteTour,
-    aliasTopTours
+    aliasTopTours,
+    getTourStats
 } = require("../controllers/tourController");
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
