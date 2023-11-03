92971fc0f8d4730a230cf652b60b6d3d59fde2ea
Viktor <viktor.toman19@gmail.com>
2023-11-03 14:03:17 +0100

    add: query middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -53,7 +53,11 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 }, {
     toJSON: {virtuals: true},
     toObject: {virtuals: true}
@@ -70,6 +74,18 @@ tourSchema.pre("save", function (next) {
 
 tourSchema.post("save", function (doc, next) {
     console.log(doc);
+    next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+    this.find({secretTour: {$ne: true}});
+    this.start = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds`);
+    console.log(docs);
     next();
 });
 
