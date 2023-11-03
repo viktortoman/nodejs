4ba52a594d322ab3b8b10a34ef5669b6451561df
Viktor <viktor.toman19@gmail.com>
2023-11-03 13:17:37 +0100

    add: aggregation pipeline (unwin, project)

--- 4-natours/starter/routes/tourRoutes.js
+++ 4-natours/starter/routes/tourRoutes.js
@@ -6,13 +6,15 @@ const {
     getTour,
     deleteTour,
     aliasTopTours,
-    getTourStats
+    getTourStats,
+    getMonthlyPlan
 } = require("../controllers/tourController");
 
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
 router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
