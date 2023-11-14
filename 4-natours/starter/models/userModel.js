df72b368e0608bac850460096df0397a39fdec16
vtomanInstructure <viktor.toman@instructure.com>
2023-11-14 17:49:38 +0100

    add: userModel

---
+++ 4-natours/starter/models/userModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A user must have a name'],
+        trim: true,
+        maxlength: [40, 'A user name must have less or equal then 40 characters'],
+        minlength: [10, 'A user name must have more or equal then 10 characters']
+    },
+    email: {
+        type: String,
+        required: [true, 'A user must have a email'],
+        unique: true,
+        lowercase: true
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'A user must have a password'],
+        minlength: [8, 'A user password must have more or equal then 8 characters']
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'A user must have a passwordConfirm'],
+        validate: {
+            // This only works on CREATE and SAVE!!!
+            validator: function(el) {
+                return el === this.password;
+            },
+            message: 'Password are not the same!'
+        }
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
